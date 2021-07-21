b06ed9102d392e265e7b55c3709695014192542c
yooooyo <loipswqe@gmail.com>
2021-07-21 16:49:33 +0800

    Feat: add serializer's depth

--- lab_management/cat/serializers.py
+++ lab_management/cat/serializers.py
@@ -58,14 +58,17 @@ class GeneralQueryStringSerializer(serializers.ModelSerializer):
         model = GeneralQueryString
         fields='__all__'
 
-class TaskIssueSerializer(serializers.ModelSerializer):
+class DynamicSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        self.Meta.depth = kwargs.pop("depth", 0)
+        super().__init__(*args, **kwargs)
+
+class TaskIssueSerializer(DynamicSerializer):
 
     class Meta:
         model = TaskIssue
         fields='__all__'
     
-    def create(self, validated_data):
-        return super().create(validated_data)
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title',instance.title)
@@ -77,19 +80,12 @@ class TaskIssueSerializer(serializers.ModelSerializer):
         instance.description = validated_data.get('description',instance.description)
         return super().update(instance, validated_data)
 
-class TaskSerializer(serializers.ModelSerializer):
-    def __init__(self, *args, **kwargs):
-        depth = kwargs.pop("depth", None)
-        if depth:
-            self.Meta.depth = depth
-        super(TaskSerializer, self).__init__(*args, **kwargs)
+class TaskSerializer(DynamicSerializer):
 
     class Meta:
         model = Task
         fields='__all__'
     
-    def create(self, validated_data):
-        return Task.objects.create(**validated_data)
 
     def update(self,instance,validated_data):
         instance.uut = validated_data.get('uut',instance.uut)
